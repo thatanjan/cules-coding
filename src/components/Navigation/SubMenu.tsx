48409943f0d34479b9c2707fef699151df613704
thatanjan <anjan.cules@gmail.com>
2021-04-19 11:22:22 +0600

    react key is generated in SubMenu

--- src/components/Navigation/SubMenu.tsx
+++ src/components/Navigation/SubMenu.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { nanoid } from 'nanoid'
 
 interface Props {
 	menuName: string
@@ -11,7 +12,7 @@ const SubMenu = ({ menuName, subMenuList }: Props) => {
 	return (
 		<ul className='sub-menu' style={{ display: 'block' }}>
 			{subMenuList.map(subMenu => (
-				<li>
+				<li key={nanoid()}>
 					<a href={generatePath(subMenu)}>{subMenu}</a>
 				</li>
 			))}
