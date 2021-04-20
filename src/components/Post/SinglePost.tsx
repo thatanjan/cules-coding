8e3a1fe5a2cf3f2ee3a85be359d26494425d9235
thatanjan <anjan.cules@gmail.com>
2021-04-20 18:13:14 +0600

    a single post component created for preview post. With that component a masonary layout has been created for testing. And also some unnecessary css has been removed from main.css files

---
+++ src/components/Post/SinglePost.tsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import NextImage from 'next/image'
+
+interface Props {}
+
+const SinglePost = (props: Props) => {
+	return (
+		<>
+			<article className='masonry__brick entry format-standard '>
+				<div className='entry__thumb'>
+					<a href='single-standard.html' className='entry__thumb-link'>
+						<NextImage
+							layout='responsive'
+							height={1080}
+							width={1080}
+							src='/ts.jpg'
+							alt='you fucking fucker'
+						/>
+					</a>
+				</div>
+
+				<div className='entry__text'>
+					<div className='entry__header'>
+						<h2 className='entry__title'>
+							<a href='single-standard.html'>Just a Standard Format Post.</a>
+						</h2>
+						<div className='entry__meta'>
+							<span className='entry__meta-cat'>
+								<a href='category.html'>Design</a>
+								<a href='category.html'>Photography</a>
+							</span>
+							<span className='entry__meta-date'>
+								<a href='single-standard.html'>Apr 29, 2019</a>
+							</span>
+						</div>
+					</div>
+					<div className='entry__excerpt'>
+						<p>
+							Lorem ipsum Sed eiusmod esse aliqua sed incididunt aliqua incididunt
+							mollit id et sit proident dolor nulla sed commodo est ad minim elit
+							reprehenderit nisi officia aute incididunt velit sint in aliqua...
+						</p>
+					</div>
+				</div>
+			</article>
+		</>
+	)
+}
+
+export default SinglePost
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,35 @@
+import Masonry from 'react-masonry-css'
+
+import SinglePost from 'components/Post/SinglePost'
+
 const Home = () => {
-	return <div>Hello world</div>
+	const breakpointColumnsObj = {
+		default: 3,
+		1500: 2,
+		800: 1,
+	}
+
+	return (
+		<>
+			<div className='s-content'>
+				<div className='masonry-wrap'>
+					<div className='masonry'>
+						<Masonry
+							breakpointCols={breakpointColumnsObj}
+							className='my-masonry-grid'
+							columnClassName='my-masonry-grid_column'
+						>
+							{Array(20)
+								.fill(3)
+								.map((_, index) => {
+									return <SinglePost key={index} />
+								})}
+						</Masonry>
+					</div>
+				</div>
+			</div>
+		</>
+	)
 }
 
 export default Home
