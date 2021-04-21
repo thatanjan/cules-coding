cc0b82085b597d0ee9ce3bd5e0b64700ef401d34
thatanjan <anjan.cules@gmail.com>
2021-04-21 08:23:33 +0600

    checking if the request is always post

--- src/pages/api/admin/create-post.ts
+++ src/pages/api/admin/create-post.ts
@@ -5,6 +5,12 @@ import BlogModel from 'mongoose/Blog'
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 	await connectDB()
 
+	const { method, body } = req
+
+	console.log(body)
+	if (method !== 'POST')
+		return res.status(400).json({ errorMessage: 'no route other than post' })
+
 	res.status(200).json({ name: 'John Doe' })
 }
 
