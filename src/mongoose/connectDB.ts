bf970b5e7ef4485f455adc00aa8d95acdaa42ef9
thatanjan <anjan.cules@gmail.com>
2021-04-21 07:48:37 +0600

    connectDB fuction will create a connection to mongodb server

---
+++ src/mongoose/connectDB.ts
@@ -0,0 +1,40 @@
+import mongoose from 'mongoose'
+
+const { MONGO_URI } = process.env
+
+if (!MONGO_URI) {
+	throw new Error('No connection string availabel on environment vatiable')
+}
+
+const globalAny: any = global
+
+let cached = globalAny.mongoose as any
+
+if (!cached) {
+	cached = globalAny.mongoose = { conn: null, promise: null }
+}
+
+async function dbConnect() {
+	if (cached.conn) {
+		return cached.conn
+	}
+
+	if (!cached.promise) {
+		const opts = {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+			bufferCommands: false,
+			bufferMaxEntries: 0,
+			useFindAndModify: false,
+			useCreateIndex: true,
+		}
+
+		cached.promise = mongoose.connect(MONGO_URI, opts).then(mongoose => {
+			return mongoose
+		})
+	}
+	cached.conn = await cached.promise
+	return cached.conn
+}
+
+export default dbConnect
