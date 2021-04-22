62de3e0bfccf5175768afebfd50e9838f2754229
thatanjan <anjan.cules@gmail.com>
2021-04-22 07:35:26 +0600

    exporting a  list of components that will be used in mdx as a object

---
+++ src/components/Blog/MDXComponents.ts
@@ -0,0 +1,6 @@
+import NextImage from 'next/image'
+import { MdxRemote } from 'next-mdx-remote/types'
+
+const mdxComponents: MdxRemote.Components = { img: NextImage }
+
+export default mdxComponents
