653782d9cb9904ff69709e10d248c7b7094fdd81
thatanjan <anjan.cules@gmail.com>
2021-04-22 17:35:04 +0600

    date property is remove from props and brought createdAt string property which will have the date as string

--- src/components/Blog/BlogHeader.tsx
+++ src/components/Blog/BlogHeader.tsx
@@ -2,12 +2,12 @@ import React from 'react'
 import Link from 'next/link'
 
 interface Props {
-	date: Date | string
+	createdAt: string
 	catagory: string
 	title: string
 }
 
-const BlogHeader = ({ title, date, catagory }: Props) => {
+const BlogHeader = ({ title, createdAt, catagory }: Props) => {
 	return (
 		<div className='content__page-header entry__header'>
 			<h1 className='display-1 entry__title'>{title}</h1>
@@ -15,7 +15,7 @@ const BlogHeader = ({ title, date, catagory }: Props) => {
 				<li className='author'>
 					By <Link href='https://anjan.vercel.app/'>Anjan Shomodder</Link>
 				</li>
-				<li className='date'>{date}</li>
+				<li className='date'>{createdAt}</li>
 				<li className='cat-links'>
 					<Link href={`/catagory/${catagory}`}>{catagory}</Link>
 				</li>
