c814aa28e4a7ac6eba0e945ff4115253e94bb541
thatanjan <anjan.cules@gmail.com>
2021-04-23 08:50:22 +0600

    getFilesByDate will read the files in a directory and will return us in a date sorted order

--- src/utils/getFiles.ts
+++ src/utils/getFiles.ts
@@ -7,4 +7,25 @@ const getFiles = (dirPath: string) => {
 	return fs.readdirSync(path.join(root, 'src/blogs/catagories', dirPath))
 }
 
+export const getFilesByDate = (
+	dirPath: string,
+	order: 'asscending' | 'descending'
+) => {
+	return getFiles(dirPath)
+		.map(fileName => ({
+			name: fileName,
+			time: fs
+				.statSync(
+					process.cwd() + '/src/blogs/catagories/' + dirPath + '/' + fileName
+				)
+				.mtime.getTime(),
+		}))
+		.sort((a, b) => {
+			if (order === 'asscending') return a.time - b.time
+
+			return b.time - a.time
+		})
+		.map(v => v.name)
+}
+
 export default getFiles
