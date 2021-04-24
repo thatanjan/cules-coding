9b6c3fe2764f7f10958083ebe8664b782b01c504
thatanjan <anjan.cules@gmail.com>
2021-04-24 21:30:12 +0600

    reading all file from blogs and updating to the database

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,35 +1,75 @@
-import Masonry from 'react-masonry-css'
+import { GetStaticProps } from 'next'
+import matter from 'gray-matter'
 
-import SingleBlog from 'components/Blog/SingleBlog'
+import connectDB from 'mongoose/connectDB'
 
-const Home = () => {
-	const breakpointColumnsObj = {
-		default: 3,
-		1500: 2,
-		800: 1,
-	}
+import BlogModel from 'mongoose/Blog'
+import getFiles from 'utils/getFiles'
+import readFilesBySlug from 'utils/readFilesBySlug'
 
+const Home = ({ slugs }: any) => {
 	return (
 		<>
-			<div className='s-content'>
-				<div className='masonry-wrap'>
-					<div className='masonry'>
-						<Masonry
-							breakpointCols={breakpointColumnsObj}
-							className='my-masonry-grid'
-							columnClassName='my-masonry-grid_column'
-						>
-							{Array(20)
-								.fill(3)
-								.map((_, index) => {
-									return <SingleBlog key={index} />
-								})}
-						</Masonry>
-					</div>
-				</div>
-			</div>
+			<div className='s-content'>hello</div>
+
+			{slugs.map((slug: any) => (
+				<h1 key={slug}>{slug}</h1>
+			))}
 		</>
 	)
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+	await connectDB()
+
+	const readOneCategoryFiles = (category: string) => {
+		const fileNames = getFiles(category)
+
+		const fileDatas = fileNames.map(fileName =>
+			readFilesBySlug(category, fileName)
+		)
+
+		const matterDatas = fileDatas.map(fileData => matter(fileData))
+
+		const returnObject = matterDatas.map(({ content, data }, index) => {
+			const slug = fileNames[index].replace('.mdx', '')
+
+			return {
+				content,
+				...data,
+				title: slug.replace('-', ' '),
+				category,
+				slug,
+			}
+		})
+
+		return returnObject
+	}
+
+	const categories = getFiles('')
+
+	let data = []
+
+	categories.forEach(category => {
+		data = data.concat(readOneCategoryFiles(category))
+	})
+
+	const promises = data.map(blog => {
+		return BlogModel.updateOne(
+			{ title: blog.title },
+			{ $set: blog },
+			{
+				upsert: true,
+				setDefaultsOnInsert: true,
+			}
+		)
+	})
+
+	await Promise.all(promises)
+
+	return {
+		props: { slugs: [] },
+	}
 }
 
 export default Home
