f2aac2cf16fd2c4ac2134ced6a1122c0cc0a75af
thatanjan <anjan.cules@gmail.com>
2021-04-25 07:17:44 +0600

    Category model will store the data about a specific category.

---
+++ src/mongoose/Category.ts
@@ -0,0 +1,20 @@
+import mongoose, { Schema, model } from 'mongoose'
+
+const schema = new Schema({
+	title: {
+		type: String,
+		required: true,
+	},
+	description: {
+		type: String,
+		required: true,
+	},
+	slug: {
+		type: String,
+		required: true,
+	},
+	banner: { type: String, required: true },
+	altText: { type: String, required: true },
+})
+
+export default mongoose.models.category || model('category', schema)
