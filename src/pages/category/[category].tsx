f034edd6ab28bd006d5fae82c4fd25e33ba623f3
thatanjan <anjan.cules@gmail.com>
2021-04-25 06:46:00 +0600

    Getting all the post from database sorted by date is passed to the component as props

--- src/pages/category/[category].tsx
+++ src/pages/category/[category].tsx
@@ -17,13 +17,14 @@ interface Props {
 }
 
 const category = ({ category, blogs }: Props) => {
+	console.log(blogs)
 	return (
 		<>
-			<header className='listing-header'>
-				<h1 className='h2'>Category: {category}</h1>
-			</header>
+			{/* <header className='listing-header'> */}
+			{/* 	<h1 className='h2'>Category: {category}</h1> */}
+			{/* </header> */}
 
-			<MasonaryBlogs {...{ blogs }} />
+			{/* <MasonaryBlogs {...{ blogs }} /> */}
 		</>
 	)
 }
@@ -46,38 +47,21 @@ export const getStaticProps: GetStaticProps = async ({
 }: {
 	params: { category: string }
 }) => {
-	const allMatters = readAllFrontMatters(category)
-
 	await connectDB()
 
-	const updateDB = allMatters.map(({ description, title }) => {
-		const updateData = {
-			slug: title.replace(' ', '-'),
-			title,
-			description,
-			category,
-		}
-
-		return BlogModel.findOneAndUpdate(
-			{ title },
-			{ $set: updateData },
-			{
-				new: true,
-				upsert: true,
-				setDefaultsOnInsert: true,
-				projection: 'createdAt slug',
-			}
-		)
+	let response = await BlogModel.find(
+		{ category },
+		{ _id: 0, __v: 0, content: 0 }
+	).sort({
+		createdAt: -1,
 	})
 
-	const updateDBResult = await Promise.all(updateDB)
+	const blogs: Blog[] = response.map(blog => {
+		blog = blog.toObject()
+		blog.createdAt = blog.createdAt.toDateString()
 
-	const blogs: Blog[] = allMatters.map((matter, index) => ({
-		...matter,
-		createdAt: updateDBResult[index].createdAt.toDateString(),
-		slug: updateDBResult[index].slug,
-		category: category,
-	}))
+		return blog
+	})
 
 	const props: Props = { blogs, category }
 
