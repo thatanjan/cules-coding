bd3f9940198f15bccadb5f196c6f20fbfa4f7279
thatanjan <anjan.cules@gmail.com>
2021-04-27 08:46:08 +0600

    code block now will have vsdark theme

--- src/components/Blog/CodeBlock.tsx
+++ src/components/Blog/CodeBlock.tsx
@@ -2,12 +2,18 @@
 
 import React from 'react'
 import Highlight, { defaultProps } from 'prism-react-renderer'
+import vsDark from 'prism-react-renderer/themes/vsDark'
 
 const CodeBlock = ({ children, className }) => {
 	const language = className.replace(/language-/, '')
 
 	return (
-		<Highlight {...defaultProps} code={children.trim()} language={language}>
+		<Highlight
+			{...defaultProps}
+			theme={vsDark}
+			code={children.trim()}
+			language={language}
+		>
 			{({ className, style, tokens, getLineProps, getTokenProps }) => (
 				<pre className={className} style={{ ...style, padding: '20px' }}>
 					{tokens.map((line, i) => (
