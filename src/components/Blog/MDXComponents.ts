3427d9ecbebc489031c14af41d5c05c173baa4a8
thatanjan <anjan.cules@gmail.com>
2021-05-03 12:55:44 +0600

    all a tag will be nextjs Link tag

--- src/components/Blog/MDXComponents.ts
+++ src/components/Blog/MDXComponents.ts
@@ -1,8 +1,13 @@
 import { MdxRemote } from 'next-mdx-remote/types'
+import Link from 'next/link'
 
 import CodeBlock from 'components/Blog/CodeBlock'
 import BlogImage from 'components/Blog/BlogImage'
 
-const mdxComponents: MdxRemote.Components = { BlogImage, code: CodeBlock }
+const mdxComponents: MdxRemote.Components = {
+	BlogImage,
+	code: CodeBlock,
+	a: Link,
+}
 
 export default mdxComponents
