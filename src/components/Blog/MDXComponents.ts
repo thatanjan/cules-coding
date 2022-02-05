84a8c0f7978e89944b891fe92513b330675f01da
thatanjan <anjan.cules@gmail.com>
2022-02-05 18:37:44 +0600

    New Feature added.
    1. Codepen embed.

--- src/components/Blog/MDXComponents.ts
+++ src/components/Blog/MDXComponents.ts
@@ -4,12 +4,14 @@ import Link from 'next/link'
 import CodeBlock from 'components/Blog/CodeBlock'
 import BlogImage from 'components/Blog/BlogImage'
 import Iframe from 'components/Iframe'
+import Codepen from 'components/Codepen'
 
 const mdxComponents: MdxRemote.Components = {
 	BlogImage,
 	code: CodeBlock,
 	a: Link,
 	Iframe,
+	Codepen: Codepen,
 }
 
 export default mdxComponents
