67ac79afec5042e600789c582a5a3d38ecfd4e63
thatanjan <anjan.cules@gmail.com>
2022-02-06 15:45:35 +0600

    Codepen component will use prop to show the default tab.

--- src/components/Codepen.tsx
+++ src/components/Codepen.tsx
@@ -2,15 +2,18 @@ import React from 'react'
 
 interface Props {
 	id: string
+	tab?: 'html' | 'css' | 'js' | 'result'
 }
 
-const Iframe = ({ id }: Props) => {
+const Iframe = ({ id, tab }: Props) => {
 	return (
 		<section className='video-container'>
 			<iframe
 				style={{ width: '100%' }}
 				scrolling='no'
-				src={`https://codepen.io/thatanjan/embed/${id}?default-tab=result&theme-id=dark`}
+				src={`https://codepen.io/thatanjan/embed/${id}?default-tab=${
+					tab || 'result'
+				}&theme-id=dark`}
 				frameBorder='no'
 				loading='lazy'
 				allowTransparency
